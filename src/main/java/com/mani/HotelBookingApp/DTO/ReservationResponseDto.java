9f8b9031bc332fe94bbbbf364df52efa8febc0b5
Manikandan <manikm9789@gmail.com>
2024-09-23 16:23:56 +0530

    miner changes

---
+++ src/main/java/com/mani/HotelBookingApp/DTO/ReservationResponseDto.java
@@ -0,0 +1,33 @@
+package com.mani.HotelBookingApp.DTO;
+
+import java.util.List;
+
+public class ReservationResponseDto {
+    private Integer totalPages;
+    private  Integer pageNo;
+    private List<ReservationDto> reservationDto;
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public List<ReservationDto> getReservationDto() {
+        return reservationDto;
+    }
+
+    public void setReservationDto(List<ReservationDto> reservationDto) {
+        this.reservationDto = reservationDto;
+    }
+}
